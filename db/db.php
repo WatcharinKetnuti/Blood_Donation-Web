f503ff624021e7b3173333e29b9d4bc0bf6515e7
WatcharinMUT <6614210011@mut.ac.th>
2025-02-01 13:33:11 +0700

    Template and DB
    Add Template and Connect to DB

---
+++ db/db.php
@@ -0,0 +1,40 @@
+<?php   
+//echo phpinfo();
+// $link= mysqli_connect('it.e-tech.ac.th','web22','4409','web22_resblood');
+
+session_start();
+$link = mysqli_connect('localhost','root','','blood_donation');
+date_default_timezone_set('Asia/Bangkok');
+mysqli_set_charset ($link,'utf8');
+
+
+function get($sql)
+{
+    global $link;
+    $res = mysqli_query($link,$sql);
+    return mysqli_fetch_all($res,MYSQLI_ASSOC);
+}
+
+function set($sql)
+{
+    global $link;
+    return mysqli_query($link,$sql);
+    
+}
+
+function authen()
+{
+	return $_SESSION['user']?? false;
+}
+function type($data)
+{
+	$authen= authen();
+	return $authen[$data]?? '';
+}
+
+function data($data)
+{
+	return $_SESSION['data'][$data]?? '';
+}
+
+?>
